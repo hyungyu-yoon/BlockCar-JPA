f7ed618bcca9dd91552ba177ff2ee53679616340
hyungyu-yoon <hyungyuyoon.dev@gmail.com>
2019-10-29 17:30:52 +0900

    init project

---
+++ src/main/java/com/bollock/blockcar/BlockCarJpaApplication.java
@@ -0,0 +1,13 @@
+package com.bollock.blockcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockCarJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockCarJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bollock/blockcar/BlockCarJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bollock.blockcar;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockCarJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
